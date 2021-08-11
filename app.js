3d8ba986cbaeb35331744be59115e934588b720d
Yosefariel <yosefariel770@gmail.com>
2021-08-11 15:18:15 +0300

    gf

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var app = express();
+"use strict";
+exports.__esModule = true;
+// var express=require('express');
+var express_1 = require("express");
+var app = express_1["default"]();
 var module;
 var morgan = require('morgan');
 app.use(morgan('dev'));
-// app.use((req,res,next)=>{
-//    res.header('Access-Control-Allow-Origin','*');
-//    res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-Type,Accept,Authorizaition');
-//  if(req.method==='OPTIONS'){
-//    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-//  return res.status(200).json({});
-//    }
-//    next();
-// });
-app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded({
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-with,Content-Type,Accept,Authorizaition');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+app.use(express_1["default"].json());
+app.use(express_1["default"].text());
+app.use(express_1["default"].urlencoded({
     extended: true
 }));
 app.get('/api/numbers/prime', function (req, res) {
@@ -27,34 +30,17 @@ app.post('/api/numbers/prime/validate', function (req, res) {
     var numarray = numArr.split(',').map(function (item) {
         return parseInt(item);
     });
-    var alphas=[];
+    var alphas = [];
     alphas = numarray;
     console.log(alphas);
     var i;
-    var myArray =[];
+    var myArray = [];
     for (i = 0; i < alphas.length; i++) {
         if (alphas[i] % 2 === 0) {
             myArray.push(false);
-        }else{
-            myArray.push(true);
         }
+        myArray.push(true);
     }
-    res.send(myArray)
+    res.send(myArray);
 });
-
 module.exports = app;
-
-
-// var alphas;
-//     alphas = numarray;
-//     console.log(alphas);
-//     var i;
-//     var myArray =[];
-//     for (i = 0; i < alphas.length - 1; i++) {
-//         if (alphas[i] % 2 === 0) {
-//             myArray.push(false);
-//         }
-//         myArray.push(true) ;
-//     }
-//     res.send(myArray)
-//     console.log(myArray);
--- app.ts
+++ app.ts
@@ -1,19 +1,20 @@
-var express=require('express');
-var app=express();
+// var express=require('express');
+import express from 'express';
+const app=express();
 var module: NodeModule;
 var morgan=require('morgan');
 
 app.use(morgan('dev'));
 
-// app.use((req,res,next)=>{
-//    res.header('Access-Control-Allow-Origin','*');
-//    res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-Type,Accept,Authorizaition');
-//  if(req.method==='OPTIONS'){
-//    res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
-//  return res.status(200).json({});
-//    }
-//    next();
-// });
+app.use((req,res,next)=>{
+   res.header('Access-Control-Allow-Origin','*');
+   res.header('Access-Control-Allow-Headers','Origin,X-Requested-with,Content-Type,Accept,Authorizaition');
+ if(req.method==='OPTIONS'){
+   res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+ return res.status(200).json({});
+   }
+   next();
+});
 
 app.use(express.json());
 app.use(express.text());
@@ -36,28 +37,19 @@ app.post('/api/numbers/prime/validate',(req,res)=>{
         return parseInt(item);
     });
 
-    var alphas:number[]; 
-    alphas=numarray;
-
+    var alphas:any=[];
+    alphas = numarray;
     console.log(alphas);
-    
-    var i:number; 
-    var b:any [];
-    for( i = 0; i < alphas.length-1; i++) {
-        if(alphas[i] % 2 === 0) {
-            // res.send(false) ;  
-            console.log(false);
-            // return i;  
-               
-        }      
-        // res.send(true) ; 
-           console.log(true);
-        // return i;  
-        b=[1,i]
-    } 
-    console.log(b);
-    
-})
+    var i;
+    var myArray =[];
+    for (i = 0; i < alphas.length; i++) {
+        if (alphas[i] % 2 === 0) {
+            myArray.push(false);
+        }
+            myArray.push(true);
+        }
+    res.send(myArray)
+});
 
 module.exports=app;
 
